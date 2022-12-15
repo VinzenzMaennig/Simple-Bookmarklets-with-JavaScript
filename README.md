43b45188953b12df08a1d330ab9a9617445cc059
Vinzenz Maennig <github@vinzenz-maennig.com>
2022-12-15 01:14:54 +0100

    changed split

--- README.md
+++ README.md
@@ -59,7 +59,7 @@ In this list bookmarks and bookmarklets I use are stored. Just copy the code int
 
 ### Google Navigation
 
-`javascript: var home = "### put in your home adress here ###"; var args = "%s".split(/[\s,]+/); var origin = ((args[0] == "home") ? home : args[0]); var destination = ((args[1] == "home") ? home : args[1]); var mode = ""; switch(args[2]) { case "c": mode = "driving"; break; case "b": mode = "bicycling"; break; case "p": mode = "transit"; } document.location.href = "https://www.google.com/maps/dir/?api=1&origin=" + origin + "&destination=" + destination + "&travelmode=" + mode;`
+`javascript: var home = "### put in your home adress here ###"; var args = "%s".split(","); var origin = ((args[0] == "home") ? home : args[0]); var destination = ((args[1] == "home") ? home : args[1]); var mode = ""; switch(args[2]) { case "c": mode = "driving"; break; case "b": mode = "bicycling"; break; case "p": mode = "transit"; } document.location.href = "https://www.google.com/maps/dir/?api=1&origin=" + origin + "&destination=" + destination + "&travelmode=" + mode;`
 
 > keyword "start", "destination", "travelmode"
 
--- google_maps_navigation.js
+++ google_maps_navigation.js
@@ -3,7 +3,7 @@
 // Usage: "your keyword", origin, destination, mode of transport (c/b/p)
 javascript:
 var home = "### put in your home adress here ###";
-var args = "%s".split(/[\s,]+/);
+var args = "%s".split(",");
 var origin = ((args[0] == "home") ? home : args[0]);
 var destination = ((args[1] == "home") ? home : args[1]);
 var mode = "";
@@ -20,17 +20,17 @@ switch(args[2]) {
 document.location.href = "https://www.google.com/maps/dir/?api=1&origin=" + origin + "&destination=" + destination + "&travelmode=" + mode;
 
 // Remember the code must be a single line without tabs and other formating to work as a Bookmarklet
-javascript: var home = "### put in your home adress here ###"; var args = "%s".split(/[\s,]+/); var origin = ((args[0] == "home") ? home : args[0]); var destination = ((args[1] == "home") ? home : args[1]); var mode = ""; switch(args[2]) { case "c": mode = "driving"; break; case "b": mode = "bicycling"; break; case "p": mode = "transit"; } document.location.href = "https://www.google.com/maps/dir/?api=1&origin=" + origin + "&destination=" + destination + "&travelmode=" + mode;
+javascript: var home = "### put in your home adress here ###"; var args = "%s".split(","); var origin = ((args[0] == "home") ? home : args[0]); var destination = ((args[1] == "home") ? home : args[1]); var mode = ""; switch(args[2]) { case "c": mode = "driving"; break; case "b": mode = "bicycling"; break; case "p": mode = "transit"; } document.location.href = "https://www.google.com/maps/dir/?api=1&origin=" + origin + "&destination=" + destination + "&travelmode=" + mode;
 
 
 // old and simpler code version not utilizing the provided Google Maps API
 javascript:(function(){
     const home = "### put in your home adress here ###";
-    var args = "%s".split(/[\s,]+/);
+    var args = "%s".split(",");
     var origin = ((args[0] == "home") ? home : args[0]);
     var destination = ((args[1] == "home") ? home : args[1]);
     document.location.href = "https://www.google.de/maps/dir/" + origin + "/" + destination;
 })();
 
 // line variant
-javascript:(function(){ const home = "### put in your home adress here ###"; var args = "%s".split(/[\s,]+/); var origin = ((args[0] == "home") ? home : args[0]); var destination = ((args[1] == "home") ? home : args[1]); document.location.href = "https://www.google.de/maps/dir/" + origin + "/" + destination; })();
+javascript:(function(){ const home = "### put in your home adress here ###"; var args = "%s".split(","); var origin = ((args[0] == "home") ? home : args[0]); var destination = ((args[1] == "home") ? home : args[1]); document.location.href = "https://www.google.de/maps/dir/" + origin + "/" + destination; })();
